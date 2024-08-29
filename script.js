4b2974f92377d67a24cdcfdfb3944265c82b647e
HuangDatt <zidanee0308@gmail.com>
2024-08-29 16:21:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const noButton = document.getElementById('no');
+    let scale = 1;
+    noButton.addEventListener('click', () => {
+        const rect = noButton.getBoundingClientRect();
+        const width = rect.width;
+        const height = rect.height;
+        if (width > 10 && height > 20) {
+            scale -= 0.2    ;
+            noButton.style.transform = `scale(${scale})`;
+        } else {
+            noButton.style.display = 'none';
+        }
+    });
+});
---
+++ web.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+    window.location.href = 'web.html';
+}, 500);
